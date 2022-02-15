ec9996d27368291a99efb30ba117caacd5cfa7ea
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2022-02-15 22:08:04 +0100

    Cool
    perfect

---
+++ Book/src/Book.java
@@ -0,0 +1,19 @@
+public class Book {
+
+    private String title;
+    private int numPages;
+
+    public Book(String title, int numPages) {
+        this.title = title;
+        this.numPages = numPages;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getNumPages() {
+        return numPages;
+    }
+
+}
