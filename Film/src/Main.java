db7a4c9a56e0a2fb369529d9e61697785de9f936
Joan Barahona <82812582+JoanBarahona@users.noreply.github.com>
2022-03-05 15:17:57 +0100

    Add files via upload

---
+++ Film/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[]args){
+        String title = askForFilmTitle();
+        String directorName = askForDirectorName();
+        boolean isThriller = askIfIsThriller();
+    }
+}
