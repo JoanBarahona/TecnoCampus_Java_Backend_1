ebe62f06e137a280385b56509517799ce4a0b1cb
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2022-03-06 19:58:15 +0100

    rocket first commit push

--- Rockets/src/Main.java
+++ Rockets/src/Main.java
@@ -8,16 +8,31 @@ public class Main {
         Integer i;
 
         System.out.println("Fase-1");
+
         Rockets rocket1 = new Rockets( "32WeSSDS", 3);
         Rockets rocket2 = new Rockets( "LDSFJA32", 6);
         System.out.println("Rocket-1: " + rocket1.name + " and have " + rocket1.numberOfPropulsors + " propulsors. ");
         System.out.println("Rocket-2: " + rocket2.name + " and have " + rocket2.numberOfPropulsors + " propulsors. ");
 
         System.out.println("Fase-2");
+
         List<Integer> rocket1Propulsor = new ArrayList<Integer>();
         rocket1Propulsor.add(10);
         rocket1Propulsor.add(30);
         rocket1Propulsor.add(80);
+
+        List<Integer>rocket2Propulsor = new ArrayList<Integer>();
+        rocket2Propulsor.add(30);
+        rocket2Propulsor.add(40);
+        rocket2Propulsor.add(50);
+        rocket2Propulsor.add(50);
+        rocket2Propulsor.add(30);
+        rocket2Propulsor.add(10);
+
+        System.out.println(rocket1.name + ": " + Rockets.toString(rocket1Propulsor));
+        System.out.println(rocket2.name + ": " + Rockets.toString(rocket2Propulsor));
+
+
     }
 
 }
